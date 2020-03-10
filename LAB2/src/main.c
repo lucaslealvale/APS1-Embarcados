f55ddcc415500da08c45e21603cf592d28a83c95
zMendes <leonardom1808@gmail.com>
2020-03-10 13:12:21 -0300

    For n funcionando

--- LAB2/src/main.c
+++ LAB2/src/main.c
@@ -2,20 +2,95 @@
 
 
 #include "asf.h"
-#define NOTE_C4  262   //Defining note frequency
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
 #define NOTE_D4  294
+#define NOTE_DS4 311
 #define NOTE_E4  330
 #define NOTE_F4  349
+#define NOTE_FS4 370
 #define NOTE_G4  392
+#define NOTE_GS4 415
 #define NOTE_A4  440
+#define NOTE_AS4 466
 #define NOTE_B4  494
 #define NOTE_C5  523
+#define NOTE_CS5 554
 #define NOTE_D5  587
+#define NOTE_DS5 622
 #define NOTE_E5  659
 #define NOTE_F5  698
+#define NOTE_FS5 740
 #define NOTE_G5  784
+#define NOTE_GS5 831
 #define NOTE_A5  880
+#define NOTE_AS5 932
 #define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
 
 // Config LED
 #define LED_PIO           PIOC                 // periferico que controla o LED
@@ -38,112 +113,58 @@
 /************************************************************************/
 /* constants                                                            */
 /************************************************************************/
-int notes[] = {       //Note of the song, 0 is a rest/pulse
-	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-	NOTE_A4, NOTE_G4, NOTE_A4, 0,
-	
-	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-	NOTE_A4, NOTE_G4, NOTE_A4, 0,
-	
-	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-	NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0,
-	NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-	NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-	
-	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-	NOTE_D5, NOTE_E5, NOTE_A4, 0,
-	NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-	NOTE_C5, NOTE_A4, NOTE_B4, 0,
+int notes[] = {
+	NOTE_E7, NOTE_E7, 0, NOTE_E7,
+	0, NOTE_C7, NOTE_E7, 0,
+	NOTE_G7, 0, 0,  0,
+	NOTE_G6, 0, 0, 0,
 
-	NOTE_A4, NOTE_A4,
-	//Repeat of first part
-	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-	NOTE_A4, NOTE_G4, NOTE_A4, 0,
+	NOTE_C7, 0, 0, NOTE_G6,
+	0, 0, NOTE_E6, 0,
+	0, NOTE_A6, 0, NOTE_B6,
+	0, NOTE_AS6, NOTE_A6, 0,
 
-	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-	NOTE_A4, NOTE_G4, NOTE_A4, 0,
-	
-	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
-	NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0,
-	NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-	NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-	
-	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
-	NOTE_D5, NOTE_E5, NOTE_A4, 0,
-	NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-	NOTE_C5, NOTE_A4, NOTE_B4, 0,
-	//End of Repeat
+	NOTE_G6, NOTE_E7, NOTE_G7,
+	NOTE_A7, 0, NOTE_F7, NOTE_G7,
+	0, NOTE_E7, 0, NOTE_C7,
+	NOTE_D7, NOTE_B6, 0, 0,
 
-	NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-	NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-	NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-	NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
+	NOTE_C7, 0, 0, NOTE_G6,
+	0, 0, NOTE_E6, 0,
+	0, NOTE_A6, 0, NOTE_B6,
+	0, NOTE_AS6, NOTE_A6, 0,
 
-	NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-	NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-	NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-	NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
+	NOTE_G6, NOTE_E7, NOTE_G7,
+	NOTE_A7, 0, NOTE_F7, NOTE_G7,
+	0, NOTE_E7, 0, NOTE_C7,
+	NOTE_D7, NOTE_B6, 0, 0
 };
 
-int duration[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 375, 125,
-	
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 375, 125,
-	
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 125, 250, 125,
+int tempo[] = {
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
 
-	125, 125, 250, 125, 125,
-	250, 125, 250, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 375, 375,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
 
-	250, 125,
-	//Rpeat of First Part
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 375, 125,
-	
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 375, 125,
-	
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 125, 250, 125,
+	9, 9, 9,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
 
-	125, 125, 250, 125, 125,
-	250, 125, 250, 125,
-	125, 125, 250, 125, 125,
-	125, 125, 375, 375,
-	//End of Repeat
-	
-	250, 125, 375, 250, 125, 375,
-	125, 125, 125, 125, 125, 125, 125, 125, 375,
-	250, 125, 375, 250, 125, 375,
-	125, 125, 125, 125, 125, 500,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
 
-	250, 125, 375, 250, 125, 375,
-	125, 125, 125, 125, 125, 125, 125, 125, 375,
-	250, 125, 375, 250, 125, 375,
-	125, 125, 125, 125, 125, 500
+	9, 9, 9,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
+	12, 12, 12, 12,
 };
 
 /************************************************************************/
@@ -198,19 +219,28 @@ int main(void)
 	// inicializa sistema e IOs
 	init();
 	
-	char status;
 
 	// super loop
 	// aplicacoes embarcadas não devem sair do while(1).
 	while (1)
 	{
+		for (int i=0;i <33;i++){
+			for (int j=0;j<200;j++){
+				pio_set(BUZ_PIO,BUZ_PIO_IDX_MASK);
+				pio_set(PIOC, LED_PIO_IDX_MASK);      
+				delay_us((1.0/(2.0*(float)notes[i]))*1000000.0);                        
+				pio_clear(PIOC, LED_PIO_IDX_MASK);   
+				pio_clear(BUZ_PIO,BUZ_PIO_IDX_MASK);
+				delay_us((1.0/(2.0*(float)notes[i]))*1000000.0);
+			}             
+		}
+		
 		pio_set(BUZ_PIO,BUZ_PIO_IDX_MASK);
-		pio_set(PIOC, LED_PIO_IDX_MASK);      // Coloca 1 no pino LED
-		delay_ms(500);                        // Delay por software de 200 ms
-		pio_clear(PIOC, LED_PIO_IDX_MASK);    // Coloca 0 no pino do LED
+		delay_us(200);
 		pio_clear(BUZ_PIO,BUZ_PIO_IDX_MASK);
-		delay_ms(500);                        // Delay por software de 200 ms
-		status = pio_get(PIOA, PIO_INPUT, BUT_PIO_IDX_MASK);
+		delay_us(200);
+
+
 	}
 	return 0;
 }
