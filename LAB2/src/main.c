38310d5392b91aea66294c17606b70daedb80cf6
zMendes <leonardom1808@gmail.com>
2020-03-12 20:51:18 -0300

    Escolher a musica funcionando, pause funcionando, tres musicas funcionando, led piscando, estruturado em structs

--- LAB2/src/main.c
+++ LAB2/src/main.c
@@ -123,11 +123,28 @@
 #define NOTE_D8  4699
 #define NOTE_DS8 4978
 
+#define MAXSIZE 200
+
 // Config LED
 #define LED_PIO           PIOC                 // periferico que controla o LED
 #define LED_PIO_ID        12                  // ID do periférico PIOC (controla LED)
 #define LED_PIO_IDX       8                    // ID do LED no PIO
 #define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+
+#define LED1_PIO			PIOA
+#define LED1_PIO_ID			ID_PIOA
+#define LED1_PIO_IDX		0
+#define LED1_PIO_IDX_MASK	(1 << LED1_PIO_IDX)
+
+#define LED2_PIO			PIOC
+#define LED2_PIO_ID			ID_PIOC
+#define LED2_PIO_IDX		30
+#define LED2_PIO_IDX_MASK	(1 << LED2_PIO_IDX)
+
+#define LED3_PIO			PIOB
+#define LED3_PIO_ID			ID_PIOB
+#define LED3_PIO_IDX		2
+#define LED3_PIO_IDX_MASK	(1 << LED3_PIO_IDX)
 
 //Condig Buzzer
 #define BUZ_PIO				PIOC 
@@ -135,16 +152,31 @@
 #define BUZ_PIO_IDX			13
 #define BUZ_PIO_IDX_MASK    (1 << BUZ_PIO_IDX)
 
-// Configuracoes do botao
+// Configuracoes do botoes
 #define BUT_PIO PIOA
 #define BUT_PIO_ID 10
 #define BUT_PIO_IDX 11
 #define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
 
+
+#define BUT1_PIO			PIOD
+#define BUT1_PIO_ID			16
+#define BUT1_PIO_IDX		28
+#define BUT1_PIO_IDX_MASK	(1u << BUT1_PIO_IDX)
+
+#define BUT3_PIO			PIOA
+#define BUT3_PIO_ID			10
+#define BUT3_PIO_IDX		19
+#define BUT3_PIO_IDX_MASK	(1u << BUT3_PIO_IDX)
+
+#define BUT2_PIO			PIOC
+#define BUT2_PIO_ID			12
+#define BUT2_PIO_IDX		31
+#define BUT2_PIO_IDX_MASK	(1u << BUT2_PIO_IDX)
 /************************************************************************/
 /* constants                                                            */
 /************************************************************************/
-int notes[] = {
+int pirate_melody[] = {
 	NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
 	NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
 	NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
@@ -197,7 +229,7 @@ int notes[] = {
 	NOTE_D5, 0, 0, NOTE_C5, 0, 0,
 	NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
 };
-int tempo[] = {
+int pirate_tempo[] = {
 	83, 83, 83, 83,
 	83, 83, 166,
 	333,
@@ -218,6 +250,68 @@ int tempo[] = {
 	100, 100, 100,
 	333, 333, 333
 };
+int underworld_melody[] = {
+	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+	NOTE_AS3, NOTE_AS4, 0,
+	0,
+	NOTE_C4, NOTE_C5, NOTE_A3, NOTE_A4,
+	NOTE_AS3, NOTE_AS4, 0,
+	0,
+	NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+	NOTE_DS3, NOTE_DS4, 0,
+	0,
+	NOTE_F3, NOTE_F4, NOTE_D3, NOTE_D4,
+	NOTE_DS3, NOTE_DS4, 0,
+	0, NOTE_DS4, NOTE_CS4, NOTE_D4,
+	NOTE_CS4, NOTE_DS4,
+	NOTE_DS4, NOTE_GS3,
+	NOTE_G3, NOTE_CS4,
+	NOTE_C4, NOTE_FS4, NOTE_F4, NOTE_E3, NOTE_AS4, NOTE_A4,
+	NOTE_GS4, NOTE_DS4, NOTE_B3,
+	NOTE_AS3, NOTE_A3, NOTE_GS3,
+	0, 0, 0
+};
+//Underwolrd tempo
+int underworld_tempo[] = {
+	83, 83, 83, 83,
+	83, 83, 166,
+	333,
+	83, 83, 83, 83,
+	83, 83, 166,
+	333,
+	83, 83, 83, 83,
+	83, 83, 166,
+	333,
+	83, 83, 83, 83,
+	83, 83, 166,
+	166, 55, 55, 55,
+	166, 166,
+	166, 166,
+	166, 166,
+	55, 55, 55, 55, 55, 55,
+	100, 100, 100,
+	100, 100, 100,
+	333, 333, 333
+};
+int starWars_melody[] = {
+	a,a,a,f,cH,a,f,cH,a,0,eH,eH,eH,fH,cH,gS,f,cH,a,0
+};
+int starWars_tempo[] = {
+	500,500,500,350,150,500,350,150,650,500,500,500,500,350,150,500,350,150,650,500
+};
+
+typedef struct {
+	int *notes;
+	int *tempo;
+	int size;
+	int order;
+} music;
+
+
+
+
+
+
 /************************************************************************/
 /* variaveis globais                                                    */
 /************************************************************************/
@@ -235,26 +329,92 @@ void init(void);
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
+void play_song(music music, char *PAUSE, char *status){
+	
+	if (music.order ==1){
+		pio_clear(LED1_PIO,LED1_PIO_IDX_MASK);
+		pio_set(LED2_PIO,LED2_PIO_IDX_MASK);
+		pio_set(LED3_PIO,LED3_PIO_IDX_MASK);
+	}
+	else if (music.order==2){
+		pio_clear(LED1_PIO,LED1_PIO_IDX_MASK);
+		pio_clear(LED2_PIO,LED2_PIO_IDX_MASK);
+		pio_set(LED3_PIO,LED3_PIO_IDX_MASK);
+
+	} else {
+		pio_clear(LED1_PIO,LED1_PIO_IDX_MASK);
+		pio_clear(LED2_PIO,LED2_PIO_IDX_MASK);
+		pio_clear(LED3_PIO,LED3_PIO_IDX_MASK);
+
+	}
+	
+
+		for (int i=0;i <music.size;i++){
+				if (music.notes[i] ==0){
+					pio_set(LED_PIO,LED_PIO_IDX_MASK);
+					delay_ms(music.tempo[i]);
+				} else{
+				float temp = (1.0/(float)music.notes[i])*1000.0;
+				for (int j=0;j<(music.tempo[i]/temp)*0.9;j++){
+					*status = pio_get(BUT_PIO,PIO_DEFAULT,BUT_PIO_IDX_MASK);
+					pio_clear(LED_PIO,LED_PIO_IDX_MASK);
+				
+					if (!*status){
+						//pio_clear(BUZ_PIO,BUZ_PIO_IDX_MASK);
+						*PAUSE = !*PAUSE;
+						delay_ms(700);
+						}
+					if(*PAUSE) j--; 
+					else {
+						if(music.notes[i]!=0){
+							pio_set(BUZ_PIO,BUZ_PIO_IDX_MASK);
+							delay_us(temp*1000.0);
+							pio_clear(BUZ_PIO,BUZ_PIO_IDX_MASK);
+							delay_us(temp*1000.0);
+						}				
+					}
+				}
+				pio_set(LED_PIO,LED_PIO_IDX_MASK);
+				delay_ms(80);
+			}
+
+		}
+	
+}
 
 // Função de inicialização do uC
 void init(void){
 	// Initialize the board clock
 	sysclk_init();
-
 	// Desativa WatchDog Timer
 	WDT->WDT_MR = WDT_MR_WDDIS;
 	// Ativa o PIO na qual o LED foi conectado
 	// para que possamos controlar o LED.
 	pmc_enable_periph_clk(LED_PIO_ID);
 	pmc_enable_periph_clk(BUZ_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
 	pmc_enable_periph_clk(BUT_PIO_ID);
-
+	//pmc_enable_periph_clk();
 
 	pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED1_PIO, LED1_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED2_PIO, LED2_PIO_IDX_MASK, 0, 0, 0);
+	pio_set_output(LED3_PIO, LED3_PIO_IDX_MASK, 0, 0, 0);
+
+
 	pio_set_output(BUZ_PIO, BUZ_PIO_IDX_MASK,0,0,0);
-	
-	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_PULLUP);
-	pio_pull_up(BUT_PIO,BUT_PIO_IDX_MASK,1);
+
+	pio_set_input(BUT_PIO, BUT_PIO_IDX_MASK, PIO_PULLUP & PIO_DEBOUNCE);
+	pio_pull_up(BUT_PIO,BUT_PIO_IDX_MASK,1);	
+
+	pio_set_input(BUT1_PIO, BUT1_PIO_IDX_MASK, PIO_PULLUP & PIO_DEBOUNCE);
+	pio_pull_up(BUT1_PIO,BUT1_PIO_IDX_MASK,1);
+
+	pio_set_input(BUT2_PIO, BUT2_PIO_IDX_MASK, PIO_PULLUP & PIO_DEBOUNCE);
+	pio_pull_up(BUT2_PIO,BUT2_PIO_IDX_MASK,1);
+
+	pio_set_input(BUT3_PIO, BUT3_PIO_IDX_MASK, PIO_PULLUP & PIO_DEBOUNCE);
+	pio_pull_up(BUT3_PIO,BUT3_PIO_IDX_MASK,1);
 	
 	//pio_pull_up(BUZ_PIO,BUZ_PIO_IDX_MASK,1);
 	
@@ -270,44 +430,57 @@ int main(void)
 	// inicializa sistema e IOs
 	init();
 	
-	char PAUSE;
+	music music1;
+	music music2;
+	music music3;
+
+	music1.notes = &starWars_melody;
+	music1.tempo = &starWars_tempo;
+	music1.size = (sizeof(starWars_melody)/sizeof(starWars_melody[0]));
+	music1.order = 1;
+
+	music2.notes = &underworld_melody;
+	music2.tempo = &underworld_tempo;
+	music2.size = (sizeof(underworld_melody)/sizeof(underworld_melody[0]));
+	music2.order = 2;
+
+	music3.notes = &pirate_melody;
+	music3.tempo = &pirate_tempo;
+	music3.size = (sizeof(pirate_melody)/sizeof(pirate_melody[0]));
+	music3.order = 3;
+
+	char status;
+	char PAUSE=0;
+	char but_status = 0;
+	pio_set(LED_PIO,LED_PIO_IDX_MASK);
+	pio_set(LED1_PIO,LED1_PIO_IDX_MASK);
+	pio_set(LED2_PIO,LED2_PIO_IDX_MASK);
+	pio_set(LED3_PIO,LED3_PIO_IDX_MASK);
 	// super loop
 	// aplicacoes embarcadas não devem sair do while(1).
-	while (1)
-	{
-	if(!pio_get(BUT_PIO,PIO_DEFAULT,BUT_PIO_IDX_MASK)){
-		
-		PAUSE = !PAUSE;
-		delay_ms(10);
+	while (1) {
+		but_status = pio_get(BUT1_PIO,PIO_DEFAULT, BUT1_PIO_IDX_MASK);
+		if (!but_status){
+			delay_ms(500);
+			play_song(music1,&PAUSE, &status);
+		}
+		but_status = pio_get(BUT2_PIO,PIO_DEFAULT, BUT2_PIO_IDX_MASK);
+		if (!but_status){
+			delay_ms(500);
+			play_song(music2,&PAUSE, &status);
+		}
+		but_status = pio_get(BUT3_PIO,PIO_DEFAULT, BUT3_PIO_IDX_MASK);
+		if (!but_status){
+			delay_ms(500);
+			play_song(music3,&PAUSE, &status);
 		}
 		
-		for (int i=0;i <(sizeof(notes)/sizeof(notes[0]));i++){
-			float temp = (1.0/(float)notes[i])*1000.0;
-			for (int j=0;j<tempo[i]/temp;j++){
-				if(pio_get(BUT_PIO,PIO_DEFAULT,BUT_PIO_IDX_MASK)){
-				
-				if(notes[i]!=0){
-					
-					pio_set(BUZ_PIO,BUZ_PIO_IDX_MASK);
-					pio_set(PIOC, LED_PIO_IDX_MASK);
-					delay_us(temp*1000.0);
-					pio_clear(PIOC, LED_PIO_IDX_MASK);
-					pio_clear(BUZ_PIO,BUZ_PIO_IDX_MASK);
-					delay_us(temp*1000.0);
-			}else{
-				delay_us(temp*1000.0);
-			}}else{
-				
 			
-}
-				
-				}
-				          
-		}
-		}
 
-
+		//play_song(music2,&STOP,&PAUSE, &status);
+		//play_song(music3,&STOP,&PAUSE, &status);
 	
-
+		}
+			
 	return 0;
 }
